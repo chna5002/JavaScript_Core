c539e875f93ac09cba104209d659ea4c8e939604
chna5002 <65600303+chna5002@users.noreply.github.com>
2020-05-20 15:07:59 +0300

    Update js.js

--- HW_task1/script/js.js
+++ HW_task1/script/js.js
@@ -1,1 +1,32 @@
-alert("Nazar");
+/* TASK2 */
+console.log("Chornous");
+
+/* TASK6 */
+let firstName= "Nazar";
+let lastName= "Chornous";
+let studyGroup= "510JavaScriptCore";
+let yearOfBirth= 1993;
+let maritalStatus= true;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof studyGroup);
+console.log(typeof yearOfBirth);
+console.log(typeof maritalStatus);
+
+console.log(yearOfBirth, maritalStatus, firstName);
+
+let address= null;
+let salary;
+console.log(typeof address);
+console.log(typeof salary);
+
+/* TASK8 */
+document.write("<strong>" + "<<--TASK8-->>" + "</strong>"+ "</br>")
+let secInHour = 60*60;
+let secInDate = secInHour * 24;
+let secInMonth = secInDate * 31;
+document.write("Seconds in hour - " + secInHour + '</br>');
+document.write("Seconds in date - " + secInDate + '</br>');
+document.write("Seconds in month (31days!!!) - " + secInMonth + '</br>');
+
