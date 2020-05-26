112c31798978868558cd0931b9f723088d6b0cfa
chna5002 <65600303+chna5002@users.noreply.github.com>
2020-05-26 17:22:06 +0300

    Update js.js

--- HW_task3/script/js.js
+++ HW_task3/script/js.js
@@ -1,1 +1,20 @@
-alert("Nazar");
+/* TASK1 */
+
+let myArr = [2, 3, 4, 5];
+let res = 1;
+for (let i = 0; i < myArr.length; i++) {
+	res = res * myArr[i];
+}
+console.log(res);
+
+let q = 0;
+let res1 = 1;
+while ( q < myArr.length) {
+	res1*=myArr[q];
+	q++;
+}
+console.log(res1);
+
+
+/* TASK2 */
+
