dcaea1c91a25f12ad2bb4aadcd0a348b33945384
chna5002 <65600303+chna5002@users.noreply.github.com>
2020-05-27 00:55:37 +0300

    Update js.js

--- HW_task3/script/js.js
+++ HW_task3/script/js.js
@@ -1,5 +1,5 @@
 /* TASK1 */
-
+/*
 let myArr = [2, 3, 4, 5];
 let res = 1;
 for (let i = 0; i < myArr.length; i++) {
@@ -17,4 +17,47 @@ console.log(res1);
 
 
 /* TASK2 */
+/*
+for (let i = 0; i < 16; i++) {
+	if (i % 2 === 0) {
+		alert(i + " -- is even");
+	} else {
+		alert(i + " -- is odd");
+	}
+}
 
+
+/*TASK3*/
+/*
+let k = new Array(5);
+function randArray() {
+	for (let i = 0; i < k.length; i++) {
+		k[i] = Math.floor((Math.random() * (500-1+1))+1);
+	}
+	console.log(k);
+}
+randArray(k);
+
+
+/* TASK4 */
+/*
+function raiseToDegree() {
+	let a = Number(prompt("Enter your number : "));
+	let b = Number(prompt("Enter your degree for number : "));
+	let res4 = parseInt(a) ** parseInt(b);
+		console.log( "Your result : " + res4);
+		
+}
+raiseToDegree();
+
+
+/* TASK5 */
+
+function findMin() {
+	let min = 2;
+	for (let i = 0; i < arguments.length; i++) {
+		min *= arguments[i]
+	}
+	console.log("Min element is :  " + min);
+}
+findMin(3, 5, 8, -3, 0.2);
