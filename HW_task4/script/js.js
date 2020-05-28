2210c8c57a2ea880ff530746166d7f04344331d1
chna5002 <65600303+chna5002@users.noreply.github.com>
2020-05-28 23:32:34 +0300

    Update js.js

--- HW_task4/script/js.js
+++ HW_task4/script/js.js
@@ -1,1 +1,23 @@
-alert("Nazar");
+/* TASK1 */
+
+function calcReactangleArea() {
+	let width = Number( prompt("Task1--> Enter width : "));
+	let height = Number( prompt("Task1--> Enter height : "));
+	let s = height * width;
+		if ((isNaN(width)) || (isNaN(height)) ) {
+			throw new Error ("width or height is not a NUMBER");
+		}
+		return s;
+
+}
+try {
+
+	console.log( calcReactangleArea());
+} catch (exception) {
+	console.log(exception.name);
+	console.log(exception.message);
+	console.log(exception.stack);
+}
+
+/* TASK2 */
+
