a677a094831f8779b3551f4fc4415b37f668b043
chna5002 <65600303+chna5002@users.noreply.github.com>
2020-06-10 16:26:58 +0300

    Update js.js

--- HW_task7/script/js.js
+++ HW_task7/script/js.js
@@ -1,1 +1,39 @@
-alert("Nazar");
+/* TASK1 */
+/*
+let openedWindow;
+
+function openWindow() {
+	openedWindow = window.open("", "", "width=300, height=300");
+}
+openWindow();
+
+function changeSize() {
+	 openedWindow.resizeTo(500, 500);
+}
+setTimeout(changeSize, 2000);
+
+function moveWindow() {
+	openedWindow.moveTo(200, 200);
+}
+setTimeout(moveWindow, 4000);
+
+function closeOpenedWindow() {
+	openedWindow.close();
+}
+setTimeout(closeOpenedWindow, 6000);
+*/
+
+/* TASK2 */
+
+function changeCSS() {
+	 let changeStyle = document.getElementById("text");
+	 changeStyle.style.color = 'orange';
+	 changeStyle.style.fontSize = "20px";
+	 changeStyle.style.fontFamily = "Comic Sans MS";
+
+}
+document.querySelector("button").onclick = changeCSS;
+
+
+/* TASK3 */
+
