c8060b65ce40192dec01d9a1a316ac9b2180805c
chna5002 <65600303+chna5002@users.noreply.github.com>
2020-06-11 21:15:51 +0300

    Update js.js

--- HW_task8/script/js.js
+++ HW_task8/script/js.js
@@ -1,1 +1,12 @@
-alert("Nazar");
+function upperCase(b) {
+	let regExp = /^[A-Z]/;
+	if (!regExp.test(b)) {
+		console.log("String's not starts with uppercase ")
+	} else {
+		console.log("String's starts with uppercase character")
+	}
+	//return;
+}
+
+upperCase('Regexp');
+  
