6dd014e9ee3bb5e46d77a0ca5fca78816a9cc4c1
chna5002 <65600303+chna5002@users.noreply.github.com>
2020-06-18 00:08:28 +0300

    Update js.js

--- HW_task9/script/js.js
+++ HW_task9/script/js.js
@@ -1,1 +1,23 @@
-alert("Nazar");
+ /* TASK1 */
+
+ $("a[href^='http']").attr('target','_blank');
+
+
+ /* TASK2 */
+
+ $('h2.head').css("background","lime");
+ $('h2.head').find('.inner').css("fontSize", "35px");
+
+
+ /* TASK3 */
+
+ $( "div:first" ).insertBefore("h3:first");
+$( "div:eq(1)" ).insertBefore("h3:eq(1)");
+
+
+/* TASK4 */
+
+$("#ChkForm input:checkbox").click(function() {
+  if($("#ChkForm input:checked").length==3){
+  	 $("#ChkForm input[type=checkbox]").attr("disabled", true);
+ }});
